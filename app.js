f74f9714c728c8aa11282db31b468be1d8c741e2
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-05-25 14:46:28 +0200

    more songs

--- app.js
+++ app.js
@@ -1,37 +1,64 @@
-
 let isPlaying = false;
+let currentSongIndex = 0;
+let playlist = [];
+
 const audioPlayer = document.getElementById('audioPlayer');
 const title = document.getElementById('title');
 
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const files = event.target.files;
+    if (files.length > 0) {
+        playlist = Array.from(files);
+        currentSongIndex = 0;
+        loadSong(currentSongIndex);
+    }
+});
+
+audioPlayer.addEventListener('ended', function() {
+    nextSong();
+});
 
 function playPause() {
-    if(isPlaying === false) {
-        audioPlayer.play();
-        isPlaying = true;
-        console.log("Play");
-    }else{
+    if (isPlaying) {
         audioPlayer.pause();
         isPlaying = false;
         console.log("Pause");
+    } else {
+        audioPlayer.play();
+        isPlaying = true;
+        console.log("Play");
     }
 }
 
-
-
-document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
+function loadSong(index) {
+    if (index >= 0 && index < playlist.length) {
+        const file = playlist[index];
         const objectURL = URL.createObjectURL(file);
         audioPlayer.src = objectURL;
         audioPlayer.play();
         isPlaying = true;
-        title.innerHTML = splitVal(file.name + '', '.', '0');
+        title.innerHTML = splitVal(file.name, '.', 0);
     }
-});
+}
 
+function nextSong() {
+    if (currentSongIndex < playlist.length - 1) {
+        currentSongIndex++;
+        loadSong(currentSongIndex);
+    }
+}
+
+function prevSong() {
+    if (currentSongIndex > 0) {
+        currentSongIndex--;
+        loadSong(currentSongIndex);
+    }
+}
 
 function splitVal(val, marker, pos) {
     const elem = val.split(marker);
-    const retVal = elem[pos];
-    return retVal;
+    return elem[pos];
 }
+
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio#loop
