d910b8806c39f38d2a52ea613396c5d093364a64
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-05-25 13:04:49 +0200

    git init

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var isPlaying = false;
+const audioPlayer = document.getElementById('audioPlayer');
+const title = document.getElementById('title');
+
+
+function playPause() {
+    if(isPlaying == false) {
+        audioPlayer.play();
+        isPlaying = true;
+        console.log("Play");
+    }else{
+        audioPlayer.pause();
+        isPlaying = false;
+        console.log("Pause");
+    }
+}
+
+
+
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const objectURL = URL.createObjectURL(file);
+        audioPlayer.src = objectURL;
+        audioPlayer.play();
+        isPlaying = true;
+        console.log(file);
+        title.innerHTML = splitVal(file.name + '', '.', '0');
+    }
+});
+
+function splitVal(val, marker, pos) {
+    const elem = val.split(marker);
+    const retVal = elem[pos];
+    return retVal;
+}
